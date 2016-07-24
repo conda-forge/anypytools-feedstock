a0a281ce057ba2081edcf876260c1b29143698d2
Morten Enemark Lund <melund@gmail.com>
2016-07-24 23:42:19 +0200

    Make recipe launch notebooks though a bat file. Anaconda-navigator is inherently broken.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,8 @@
 "%PYTHON%" setup.py install 
 
-cd "%PREFIX%\share"
-mkdir notebooks\AnyPyTools
-cd notebooks\AnyPyTools
+cd "%PREFIX%"
+mkdir share\notebooks\AnyPyTools
+cd share\notebooks\AnyPyTools
 xcopy "%SRC_DIR%\Tutorial" . /S/Y/I
 
 copy "%RECIPE_DIR%\AnyPyToolsTutorial.bat" "%SCRIPTS%\AnyPyToolsTutorial.bat"
