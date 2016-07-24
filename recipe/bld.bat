3d27606e219bfac5e6873f3700bfcf8fb3509c08
Morten Enemark Lund <melund@gmail.com>
2016-07-25 00:45:50 +0200

    Add windows menu entry to easily launch the tutorial

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,3 +6,9 @@ cd share\notebooks\AnyPyTools
 xcopy "%SRC_DIR%\Tutorial" . /S/Y/I
 
 copy "%RECIPE_DIR%\AnyPyToolsTutorial.bat" "%SCRIPTS%\AnyPyToolsTutorial.bat"
+cd "%PREFIX%"
+mkdir Menu
+copy "%RECIPE_DIR%\menu-windows-manual.json" "%PREFIX%\Menu\AnyPyToolsTutorial.json"
+copy "%SRC_DIR%\anypytools.ico" "%PREFIX%\Menu\anypytools.ico"
+
+cd "%SRC_DIR%"
